4e0567d8f56a8ea66a0df413aa66d36b0a085502
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-10-19 12:45:41 +0100

    added subtitle update text

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let subtitle = document.querySelector("#subtitle");
+let subtitle = document.querySelector(".subtitle");
 const lightbulb1 = document.querySelector("#lightbulb1");
 const lightbulb2 = document.querySelector("#lightbulb2");
 const lightbulb3 = document.querySelector("#lightbulb3");
@@ -7,18 +7,21 @@ let count = 0;
 
 lightbulb1.addEventListener("click", function (event) {
   lightbulb1.classList.toggle("active");
+  subtitle.innerHTML = `You've clicked the lights ${count} times!`;
   count += 1;
   console.log(count);
 });
 
 lightbulb2.addEventListener("click", function (event) {
   lightbulb2.classList.toggle("active");
+  subtitle.innerHTML = `You've clicked the lights ${count} times!`;
   count += 1;
   console.log(count);
 });
 
 lightbulb3.addEventListener("click", function (event) {
   lightbulb3.classList.toggle("active");
+  subtitle.innerHTML = `You've clicked the lights ${count} times!`;
   count += 1;
   console.log(count);
 });
