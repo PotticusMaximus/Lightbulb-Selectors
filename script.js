36591c48da461aae7884468211b02eb2d59934d5
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-10-19 12:59:25 +0100

    added style change when mouse hovers

--- script.js
+++ script.js
@@ -7,21 +7,33 @@ let count = 0;
 
 lightbulb1.addEventListener("click", function (event) {
   lightbulb1.classList.toggle("active");
-  subtitle.innerHTML = `You've clicked the lights ${count} times!`;
+  if (count === 1) {
+    subtitle.innerHTML = `You've clicked the lights ${count} time`;
+  } else {
+    subtitle.innerHTML = `You've clicked the lights ${count} times`;
+  }
   count += 1;
   console.log(count);
 });
 
 lightbulb2.addEventListener("click", function (event) {
   lightbulb2.classList.toggle("active");
-  subtitle.innerHTML = `You've clicked the lights ${count} times!`;
+  if (count === 1) {
+    subtitle.innerHTML = `You've clicked the lights ${count} time`;
+  } else {
+    subtitle.innerHTML = `You've clicked the lights ${count} times`;
+  }
   count += 1;
   console.log(count);
 });
 
 lightbulb3.addEventListener("click", function (event) {
   lightbulb3.classList.toggle("active");
-  subtitle.innerHTML = `You've clicked the lights ${count} times!`;
+  if (count === 1) {
+    subtitle.innerHTML = `You've clicked the lights ${count} time`;
+  } else {
+    subtitle.innerHTML = `You've clicked the lights ${count} times`;
+  }
   count += 1;
   console.log(count);
 });
