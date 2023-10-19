31fae5baedb3ef48459b894a92de653bbfab1f29
PotticusMaximus <136069407+PotticusMaximus@users.noreply.github.com>
2023-10-19 12:21:04 +0100

    script added and variables declared

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Write your code here
+let subtitle = document.querySelector("#subtitle");
+const light1 = document.querySelector("lightbulb1");
+const light2 = document.querySelector("lightbulb2");
+const light3 = document.querySelector("lightbulb3");
+
+let count = 0;
+
+light1.addEventListener("click", function (event) {
+  light1.classlist.toggle("active");
+  count += 1;
+  console.log(count);
+});
